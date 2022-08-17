fe96981f20960fd21a201beb21ebacb820a0dd6f
Malay Joshi <71775151+malayjoshi13@users.noreply.github.com>
2022-08-18 02:37:40 +0530

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -18,9 +18,6 @@ def create_vocab(filename):
     for col in file:
         current_sentence.append(col['Current Sentence'])
         is_oligo.append(col['Is this an oligo?'])
-    
-    # print('current_sentence:', current_sentence)
-    # print('is_oligo:', is_oligo)
 
     non_oligo_sentences = list()
     oligo_sentences = list()
@@ -28,21 +25,22 @@ def create_vocab(filename):
     oligo_sentences_words = list()
 
     for i in range(len(current_sentence)):
-        if is_oligo[i]!="Oligo":
+        if is_oligo[i]=="Oligo" and current_sentence[i] not in oligo_sentences: #appends unique sentences only
+            oligo_sentences.append(current_sentence[i])
+            for word in current_sentence[i].split():
+
+                if word not in oligo_sentences_words:
+
+                    oligo_sentences_words.append(word)           
+
+        elif is_oligo[i]!="Oligo" and current_sentence[i] not in non_oligo_sentences:
             non_oligo_sentences.append(current_sentence[i])
             for word in current_sentence[i].split():
                 if word not in non_oligo_sentences_words:
-                    non_oligo_sentences_words.append(word)            
+                    non_oligo_sentences_words.append(word) 
 
-        else:
-            oligo_sentences.append(current_sentence[i])
-            for word in current_sentence[i].split():
-                if word not in oligo_sentences_words:
-                    oligo_sentences_words.append(word)
-
-    # print(non_oligo_sentences)
-    # print(oligo_sentences)
     return non_oligo_sentences, oligo_sentences, non_oligo_sentences_words, oligo_sentences_words
+
 
 #Creating an index for each word in our vocab.
 def vocab_elements(vocab):
@@ -52,6 +50,7 @@ def vocab_elements(vocab):
         index_dict[word] = i
         i += 1
     return index_dict
+
 
 #Create a count dictionary to keep the count of the number of documents containing the given word
 def count_dict(sentences, vocab):
@@ -63,12 +62,18 @@ def count_dict(sentences, vocab):
                 word_count[word] += 1
     return word_count
 
+
 #Term Frequency
-def termfreq(sentences, word):
-    N = len(sentences)
-    occurance = len([token for token in sentences if token == word])
+def termfreq(sentence, word):
+    N = len(sentence.split())
+
+    occurance = 0
+    for token in sentence.split():
+        if token == word:
+            occurance+=1
     return occurance/N #ocurrennce of a word in a sentence/total length of that sentence
-    #passage/document has multiple sentences
+    #multiple sentences==multiple documents
+
 
 #Inverse Document Frequency
 def inverse_doc_freq(word, document_len, word_count):
@@ -78,33 +83,44 @@ def inverse_doc_freq(word, document_len, word_count):
         word_occurance = 1 
     return np.log(document_len/word_occurance) #np.log(total number of sentences in oligo passage/occurence of a word in oligo passage)
 
+
 #Combining the TF-IDF functions
-def tf_idf(sentence, vocab, document_len, word_count):
-    index_dict = vocab_elements(vocab)
-    tf_idf_vec = np.zeros((len(vocab),))
+def tf_idf(sentences, sentences_words, sentence):
+    word_count = count_dict(sentences, sentences_words)
+    document_len = len(sentences)
+
+    tf_idf_vec = {}
     for word in sentence.split():
         tf = termfreq(sentence, word)
         idf = inverse_doc_freq(word, document_len, word_count)
-        
         value = tf*idf
-        tf_idf_vec[index_dict[word]] = value 
+
+        if word not in tf_idf_vec:
+            tf_idf_vec[word] = [value]
+        else:
+            tf_idf_vec[word].append(value)
+
     return tf_idf_vec
 
 
 # main code
 filename = "oligos.csv"
 non_oligo_sentences, oligo_sentences, non_oligo_sentences_words, oligo_sentences_words = create_vocab(filename)
-total_oligo_sentences_len = len(oligo_sentences)
-total_non_oligo_sentences_len = len(non_oligo_sentences)
 
-word_count_oligo = count_dict(oligo_sentences, oligo_sentences_words)
-word_count_non_oligo = count_dict(non_oligo_sentences, non_oligo_sentences_words)
 
+tf_idf_vec_oligo_final = {}
 for oligo_sentence in oligo_sentences:
-    tf_idf_vec_oligo = tf_idf(oligo_sentence, oligo_sentences_words, total_oligo_sentences_len, word_count_oligo)
+    tf_idf_vec_oligo = tf_idf(oligo_sentences, oligo_sentences_words, oligo_sentence)
+    tf_idf_vec_oligo_final.update(tf_idf_vec_oligo)
 
-for non_oligo_sentence in non_oligo_sentences:
-    tf_idf_vec_non_oligo = tf_idf(non_oligo_sentence, non_oligo_sentences_words, total_non_oligo_sentences_len, word_count_non_oligo)
 
-print(tf_idf_vec_oligo)
-print(tf_idf_vec_non_oligo)
+# print(tf_idf_vec_oligo_final)
+for i in tf_idf_vec_oligo_final:
+    sum = 0
+    # print(str(i)+": "+str(tf_idf_vec_oligo_final[i])) #before
+
+    for j in tf_idf_vec_oligo_final[i]:
+        sum += j
+    tf_idf_vec_oligo_final[i] = sum/len(tf_idf_vec_oligo_final[i]) #if three tfidf are added then divide by 3
+    
+    print(str(i)+": "+str(tf_idf_vec_oligo_final[i])) #after
