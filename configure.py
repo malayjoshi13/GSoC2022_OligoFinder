6aab23808fb95f9301c3e3ec5d457c0e28de2b3c
Malay Joshi <71775151+malayjoshi13@users.noreply.github.com>
2022-09-07 02:07:50 +0530

    Update configure.py

--- configure.py
+++ configure.py
@@ -2,9 +2,7 @@
 import configparser
 
 def setConfiguration():
-    '''
-    Returns a dictionary with the DBconfig, Regex objects and TC pipeline
-    '''
+
     db_config = configparser.ConfigParser()
     db_config.read('utils/all_config.cfg')
 
