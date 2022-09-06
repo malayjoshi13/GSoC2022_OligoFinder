9ad92b9400c3f992979eed2d1244b1231807403b
Malay Joshi <71775151+malayjoshi13@users.noreply.github.com>
2022-09-07 02:23:30 +0530

    Update oligo_extract.py

--- oligo_extract.py
+++ oligo_extract.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import nltk
 import sys
-from configure import setConfiguration
+from Setup.configure import setConfiguration
 from RawText.get_paper_content import get_paper_sentences_with_TE
 from regex_extraction import oligo_seq_regex, oligo_name_regex
 from TP_FP_oligo.TfIdf_BOW_TpFp import check_true_positive_oligo_sentence  
