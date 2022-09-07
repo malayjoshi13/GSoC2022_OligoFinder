2620998c22b9b6e2f1ed001f9ce45775f48c476a
Malay Joshi <71775151+malayjoshi13@users.noreply.github.com>
2022-09-08 00:12:50 +0530

    Update oligo_extract.py

--- oligo_extract.py
+++ oligo_extract.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import nltk
+import sys
+from os.path import exists
 from configure import setConfiguration
 from RawText.get_paper_content import get_paper_sentences_with_TE
 from RegexRules.combine_rules import oligo_seq_regex, oligo_name_regex
-from TP_FP_oligo.TfIdf_BOW_TpFp import check_true_positive_oligo_sentence  
+from TP_FP_oligo.TfIdf_BOW_TpFp import check_true_positive_oligo_sentence
+from TP_FP_oligo.TfIdf_BOW_creator import main_fn  
 
 nltk.download("stopwords")
 nltk.download("punkt")
 
 
-def find_Oligos(config, paper_ids):
+def find_Oligos(config, paper_ids, skip_TP_FP):
     """
     Functionality:
     Compile extracted oligos (from research paper(s)), alongwith paper id and sentence
@@ -30,14 +33,21 @@ def find_Oligos(config, paper_ids):
     Next_Sentence = list()
     Number_of_Oligos = list()
     Oligo_name = list()
-    TP_FP = list()
+    TP_FP_auto = list()
+    TP_FP_manual = list()
 
     for i in range(len(paperid_sentence_list)):
             
             oligo_seq_output = oligo_seq_regex(paperid_sentence_list[i][1]) # paperid_sentence_list[i][1] means (i)^th element of list "paperid_sentence_list", then choosing first part of this element which is a sentence
             oligo_name_output = oligo_name_regex(oligo_seq_output, paperid_sentence_list[i][1])
-            true_positive_oligo_sentence = check_true_positive_oligo_sentence(paperid_sentence_list[i][1])
-            
+
+            # run auto TP_FP feature only when BOW is present
+            # i.e if CSV files exists and no matter curator has done re-curation or not, just do auto TP_FP tagging using BOW
+            if skip_TP_FP=="False":
+                true_positive_oligo_sentence = check_true_positive_oligo_sentence(paperid_sentence_list[i][1])
+            else:
+                true_positive_oligo_sentence = None
+
             if oligo_seq_output: # continue further if current sentence (i.e paperid_sentence_list[i][1])
                                  # has some oligo sequences
                     for output in oligo_seq_output:
@@ -61,24 +71,29 @@ def find_Oligos(config, paper_ids):
 
                             Number_of_Oligos.append(len(oligo_seq_output))
 
-                            TpFp = ""
+                            TpFp_auto = ""
                             if true_positive_oligo_sentence=="oligo": 
                                 # if sentence in which oligos are present is a true positive 
                                 # oligo sentence
-                                TpFp = "true positive"
+                                TpFp_auto = "true positive"
                             elif true_positive_oligo_sentence=="non-oligo":
-                                TpFp = "false positive"
-                            TP_FP.append(TpFp)
-                   
+                                TpFp_auto = "false positive"
+                            else:
+                                pass
+                            TP_FP_auto.append(TpFp_auto)
 
-    #final = {'WBPaperID': WBPaperID, 'Oligo': Oligo, 'Oligo Name': Oligo_name, 'Prev Sentence': Prev_Sentence, 'Current Sentence': Current_Sentence, 'Next Sentence': Next_Sentence, 'Oligos per Sentence': Number_of_Oligos}
-    final = {'WBPaperID': WBPaperID, 'Oligo': Oligo, 'Prev Sentence': Prev_Sentence, 'Current Sentence': Current_Sentence, 'Next Sentence': Next_Sentence, 'Oligos per Sentence': Number_of_Oligos, 'Is this an oligo (auto)': TP_FP}
+                            TpFp_manual = ""
+                            TP_FP_manual.append(TpFp_manual)
+
+                            
+    final = {'WBPaperID': WBPaperID, 'Oligo': Oligo, 'Prev Sentence': Prev_Sentence, 'Current Sentence': Current_Sentence, 'Next Sentence': Next_Sentence, 'Oligos per Sentence': Number_of_Oligos, 'TP or FP Oligo (auto)': TP_FP_auto, 'TP or FP Oligo (manual)':TP_FP_manual}
     return pd.DataFrame(final)
 
 
 if __name__ == "__main__":
-    
+
     config, paper_ids, output_CSVname, _, _ = setConfiguration()
+
 #     paper_ids = ["WBPaper00003663", "WBPaper00003632", "WBPaper00003021", "WBPaper00005504", "WBPaper00030754", "WBPaper00005177", "WBPaper00004282", "WBPaper00003566", "WBPaper00001366", "WBPaper00004943", "WBPaper00002207", "WBPaper00001691", "WBPaper00003989", "WBPaper00003632", "WBPaper00044537", "WBPaper00050743", "WBPaper00001872", "WBPaper00005135", "WBPaper00000779", "WBPaper00025193", "WBPaper00005533", "WBPaper00001366", "WBPaper00002207", "WBPaper00001691",
 # "WBPaper00050123",
 # "WBPaper00002034",
@@ -105,7 +120,19 @@ if __name__ == "__main__":
 # "WBPaper00005504",
 # "WBPaper00001835"]
 
-    df = find_Oligos(config, paper_ids)
+    # if CSV files exists, then create a new BOW using that CSV, and just do auto TP_FP tagging using BOW
+    skip_TP_FP = "True" # by default dont create BOW
+    file_exists = exists(output_CSVname)
+    print(file_exists)
+    if file_exists:
+        # here BOW will be empty if 'TP or FP Oligo (manual)' column will be empty (i.e curator has not done manual tagging).
+        # thus for the first cycle BOW will be only created if curator has done manual tagging (because BOW is formed by curator's manual tagging only)
+        main_fn() 
+        skip_TP_FP = "False"
+    elif not file_exists:
+        skip_TP_FP = "True"
+
+    df = find_Oligos(config, paper_ids, skip_TP_FP)
 
     # try:
     #     output_filename = sys.argv[1] # ex: oligos.csv
