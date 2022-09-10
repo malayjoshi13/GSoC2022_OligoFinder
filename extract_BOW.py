46e7ff985cc53ff599d01185e9f855cf6814e676
Malay Joshi <71775151+malayjoshi13@users.noreply.github.com>
2022-09-10 20:10:04 +0530

    Update extract_BOW.py

--- extract_BOW.py
+++ extract_BOW.py
@@ -3,19 +3,23 @@ from TP_FP_oligo.TfIdf_BOW_creator import create_BOW
 from oligo_extract import find_Oligos
 from os.path import exists
 
-config, paper_ids, output_CSVname, _, _ = setConfiguration()
+if __name__ == "__main__":
+    config, paper_ids, output_CSVname, _, _ = setConfiguration()
 
-# if CSV files exists, then create a new BOW using that CSV, and just do auto TP_FP tagging using BOW
-skip_TP_FP = "True" # by default dont create BOW
-file_exists = exists(output_CSVname)
+    skip_TP_FP = "True" # by default don't do auto TP/FP tagging
+    file_exists = exists(output_CSVname)
 
-if file_exists:
-    # here BOW will be empty if 'TP or FP Oligo (manual)' column will be empty (i.e curator has not done manual tagging).
-    # thus for the first cycle BOW will be only created if curator has done manual tagging (because BOW is formed by curator's manual tagging only)
-    create_BOW() 
-    skip_TP_FP = "False"
-elif not file_exists:
-    skip_TP_FP = "True"
+    if file_exists:
+        # if CSV exists but 'TP or FP Oligo (manual)' column of that CSV would not be empty (i.e curator has done manual tagging),
+        # then non-empty BOW will be created.
+        # but if CSV exists but 'TP or FP Oligo (manual)' column of that CSV would be empty (i.e curator has not done manual tagging),
+        # then empty BOW will be created.
+        create_BOW() 
+        # once BOW will be created it will be used for auto-tagging.
+        skip_TP_FP = "False"
+    elif not file_exists:
+        # if CSV don't exists, then BOW wil not be created, and thus no auto TP_FP tagging will happen
+        skip_TP_FP = "True"
 
-df = find_Oligos(config, paper_ids, skip_TP_FP)
-df.to_csv(output_CSVname, index=False)
+    df = find_Oligos(config, paper_ids, skip_TP_FP)
+    df.to_csv(output_CSVname, index=False)
