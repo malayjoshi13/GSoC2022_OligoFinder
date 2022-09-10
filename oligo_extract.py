0d27696f7f07035f69d52d55179ba8a4ad2bd18c
Malay Joshi <71775151+malayjoshi13@users.noreply.github.com>
2022-09-10 19:59:56 +0530

    Update oligo_extract.py

--- oligo_extract.py
+++ oligo_extract.py
@@ -16,8 +16,10 @@ def find_Oligos(config, paper_ids, skip_TP_FP):
     Arg:
     paper_ids - List of wb papers ids e.g. ['WBPaper00002379']
     config - Dictionary with db_config properties and texpresso token
+    skip_TP_FP - Variable with value "True" if CSV exists else "False"
     Returns:
-    Panda dataframe consisting of IDs of research paper, oligos extracted (from each sentence) and corresponding sentence
+    Panda dataframe consisting of IDs of research paper, oligos extracted (from each sentence), corresponding sentence
+    and other related data
     """
 
     paperid_sentence_list = get_paper_sentences_with_TE(config, paper_ids)
@@ -69,8 +71,7 @@ def find_Oligos(config, paper_ids, skip_TP_FP):
 
                             TpFp_auto = ""
                             if true_positive_oligo_sentence=="oligo": 
-                                # if sentence in which oligos are present is a true positive 
-                                # oligo sentence
+                                # if sentence in which oligos are present is a true positive oligo 
                                 TpFp_auto = "true positive"
                             elif true_positive_oligo_sentence=="non-oligo":
                                 TpFp_auto = "false positive"
@@ -84,55 +85,3 @@ def find_Oligos(config, paper_ids, skip_TP_FP):
                             
     final = {'WBPaperID': WBPaperID, 'Oligo': Oligo, 'Prev Sentence': Prev_Sentence, 'Current Sentence': Current_Sentence, 'Next Sentence': Next_Sentence, 'Oligos per Sentence': Number_of_Oligos, 'TP or FP Oligo (auto)': TP_FP_auto, 'TP or FP Oligo (manual)':TP_FP_manual}
     return pd.DataFrame(final)
-
-
-# if __name__ == "__main__":
-
-#     config, paper_ids, output_CSVname, _, _ = setConfiguration()
-
-# #     paper_ids = ["WBPaper00003663", "WBPaper00003632", "WBPaper00003021", "WBPaper00005504", "WBPaper00030754", "WBPaper00005177", "WBPaper00004282", "WBPaper00003566", "WBPaper00001366", "WBPaper00004943", "WBPaper00002207", "WBPaper00001691", "WBPaper00003989", "WBPaper00003632", "WBPaper00044537", "WBPaper00050743", "WBPaper00001872", "WBPaper00005135", "WBPaper00000779", "WBPaper00025193", "WBPaper00005533", "WBPaper00001366", "WBPaper00002207", "WBPaper00001691",
-# # "WBPaper00050123",
-# # "WBPaper00002034",
-# # "WBPaper00000779",
-# # "WBPaper00006439",
-# # "WBPaper00025193",
-# # "WBPaper00005533",
-# # "WBPaper00003989",
-# # "WBPaper00002034",
-# # "WBPaper00001677",
-# # "WBPaper00005504",
-# # "WBPaper00004503",
-# # "WBPaper00001835",
-# # "WBPaper00004282",
-# # "WBPaper00003663",
-# # "WBPaper00002207",
-# # "WBPaper00001691",
-# # "WBPaper00003632",
-# # "WBPaper00051175",
-# # "WBPaper00004275",
-# # "WBPaper00002034",
-# # "WBPaper00000779",
-# # "WBPaper00030754",
-# # "WBPaper00005504",
-# # "WBPaper00001835"]
-
-#     # if CSV files exists, then create a new BOW using that CSV, and just do auto TP_FP tagging using BOW
-#     skip_TP_FP = "True" # by default dont create BOW
-#     file_exists = exists(output_CSVname)
-#     print(file_exists)
-#     if file_exists:
-#         # here BOW will be empty if 'TP or FP Oligo (manual)' column will be empty (i.e curator has not done manual tagging).
-#         # thus for the first cycle BOW will be only created if curator has done manual tagging (because BOW is formed by curator's manual tagging only)
-#         main_fn() 
-#         skip_TP_FP = "False"
-#     elif not file_exists:
-#         skip_TP_FP = "True"
-
-#     df = find_Oligos(config, paper_ids, skip_TP_FP)
-
-#     # try:
-#     #     output_filename = sys.argv[1] # ex: oligos.csv
-#     # except:
-#     #     output_filename = "oligos.csv"
-
-#     df.to_csv(output_CSVname, index=False)
